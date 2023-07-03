5831600d610a7527f3d17d673994727e5ea46980
Sander Van Noten <sander.vannoten@student.hogent.be>
2023-07-03 13:44:34 +0200

    azureAD login

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-// ./src/App.tsx
-
 import React, { useState, useEffect } from "react";
+import { PublicClientApplication } from "@azure/msal-browser";
 import uploadFileToBlob, {
   isStorageConfigured,
   getBlobsInContainer,
 } from "./azure-storage-blob";
 import DisplayImagesFromContainer from "./ContainerImages";
+
 const storageConfigured = isStorageConfigured();
+
+const msalConfig = {
+  auth: {
+    clientId: "8cf60b37-5dae-407d-944b-529dfc3d76a6",
+    authority: "https://login.microsoftonline.com/didits.be",
+    redirectUri: "http://localhost:3000" // Update with your app's redirect URI
+  }
+};
+
+const loginRequest = {
+  scopes: ["openid", "profile", "User.Read"]
+};
+
+const pca = new PublicClientApplication(msalConfig);
 
 const App = () => {
   // all blobs in container
@@ -20,6 +34,9 @@ const App = () => {
   const [uploading, setUploading] = useState(false);
   const [inputKey, setInputKey] = useState(Math.random().toString(36));
 
+  // User management
+  const [user, setUser] = useState(null);
+
   // *** GET FILES IN CONTAINER ***
   useEffect(() => {
     getBlobsInContainer().then((list) => {
@@ -28,46 +45,94 @@ const App = () => {
     });
   }, [fileUploaded]);
 
+  useEffect(() => {
+    const handleResponse = (response) => {
+      if (response !== null) {
+        setUser(response.account);
+      }
+    };
+
+    const login = async () => {
+      try {
+        const accounts = await pca.getAllAccounts();
+        if (accounts.length === 0) {
+          const response = await pca.loginPopup(loginRequest);
+          handleResponse(response);
+        } else if (accounts.length === 1) {
+          const response = await pca.acquireTokenSilent({
+            account: accounts[0],
+            ...loginRequest
+          });
+          handleResponse(response);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    login();
+  }, []);
+
   const onFileChange = (event) => {
     // capture file into state
     setFileSelected(event.target.files[0]);
   };
 
   const onFileUpload = async () => {
-  if (fileSelected && fileSelected.name && selectedOption) {
-    // prepare UI
-    setUploading(true);
+    if (fileSelected && fileSelected.name && selectedOption) {
+      // prepare UI
+      setUploading(true);
 
-    try {
-      // *** UPLOAD TO AZURE STORAGE ***
-      await uploadFileToBlob(fileSelected, selectedOption);
+      try {
+        // *** UPLOAD TO AZURE STORAGE ***
+        await uploadFileToBlob(fileSelected, selectedOption);
 
-      // reset state/form
-      setFileSelected(null);
-      setFileUploaded(fileSelected.name);
-      setUploading(false);
-      setInputKey(Math.random().toString(36));
-    } catch (error) {
-      // Display error message
-      console.error("Error uploading file:", error);
-      // Show error message to the user
-      alert("Only CSV files are allowed.");
-      // Reset state/form
-      setFileSelected(null);
-      setUploading(false);
-      setInputKey(Math.random().toString(36));
+        // reset state/form
+        setFileSelected(null);
+        setFileUploaded(fileSelected.name);
+        setUploading(false);
+        setInputKey(Math.random().toString(36));
+      } catch (error) {
+        // Display error message
+        console.error("Error uploading file:", error);
+        // Show error message to the user
+        alert("Only CSV files are allowed.");
+        // Reset state/form
+        setFileSelected(null);
+        setUploading(false);
+        setInputKey(Math.random().toString(36));
+      }
+    } else {
+      // No option selected
+      alert("Please choose a bank!");
     }
-  } else {
-    // No option selected
-    alert("Please choose a bank!");
-  }
-};
+  };
 
   const [selectedOption, setSelectedOption] = useState("");
-  console.log(selectedOption);
   const handleDropdownChange = (event) => {
     setSelectedOption(event.target.value);
   };
+
+  const loginWithAzureAD = async () => {
+    try {
+      const response = await pca.loginPopup(loginRequest);
+      handleResponse(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const logoutWithAzureAD = () => {
+    pca.logout();
+    setUser(null);
+  };
+
+  const handleResponse = (response) => {
+    if (response !== null) {
+      setUser(response.account);
+    }
+  };
+
   // display form
   const DisplayForm = () => (
     <div>
@@ -91,13 +156,23 @@ const App = () => {
   return (
     <div>
       <h1>Upload file to Azure Blob Storage</h1>
-      {storageConfigured && !uploading && DisplayForm()}
-      {storageConfigured && uploading && <div>Uploading</div>}
-      <hr />
-      {storageConfigured && blobList.length > 0 && (
-        <DisplayImagesFromContainer blobList={blobList} />
+      {user ? (
+        <>
+          <p>Welcome, {user.name}!</p>
+          <button onClick={logoutWithAzureAD}>Logout</button>
+          {storageConfigured && !uploading && DisplayForm()}
+          {storageConfigured && uploading && <div>Uploading</div>}
+          <hr />
+          {storageConfigured && blobList.length > 0 && (
+            <DisplayImagesFromContainer blobList={blobList} />
+          )}
+        </>
+      ) : (
+        <>
+          <button onClick={loginWithAzureAD}>Login with Azure AD</button>
+          {!storageConfigured && <div>Storage is not configured.</div>}
+        </>
       )}
-      {!storageConfigured && <div>Storage is not configured.</div>}
     </div>
   );
 };
