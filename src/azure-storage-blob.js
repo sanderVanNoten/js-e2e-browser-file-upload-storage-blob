e24796ef723c6e767edd53ab4cdfa796d7541ead
Dieter Van Noten <dieter.vannoten@didits.be>
2023-08-25 14:37:46 +0200

    poging to managed identities

--- src/azure-storage-blob.js
+++ src/azure-storage-blob.js
@@ -2,52 +2,60 @@
 
 // <snippet_package>
 // THIS IS SAMPLE CODE ONLY - NOT MEANT FOR PRODUCTION USE
+import { DefaultAzureCredential } from "@azure/identity";
 import { BlobServiceClient } from "@azure/storage-blob";
 
 const containerName = `landingzone`;
 const sasToken = process.env.REACT_APP_AZURE_STORAGE_SAS_TOKEN;
 const storageAccountName = process.env.REACT_APP_AZURE_STORAGE_RESOURCE_NAME;
+
+// const isLocalClient = 
+
 // </snippet_package>
 
-// <snippet_get_client>
-const uploadUrl = `https://${storageAccountName}.blob.core.windows.net/?${sasToken}`;
-console.log(uploadUrl);
+// <snippet_get_upload_url>
+const uploadLocalUrl = `https://${storageAccountName}.blob.core.windows.net/?${sasToken}`;
+const uploadCloudUrl = `https://${storageAccountName}.blob.core.windows.net`
 
-// get BlobService = notice `?` is pulled out of sasToken - if created in Azure portal
-const blobService = new BlobServiceClient(uploadUrl);
+// Create Two Blob Service Clients depending on local/cloud development
+const blobServiceClientLocal = new BlobServiceClient(uploadLocalUrl);
+const blobServiceClientCloud = new BlobServiceClient(uploadCloudUrl,defaultAzureCredential);
 
 // get Container - full public read access
-const containerClient = blobService.getContainerClient(containerName);
+const containerClient = blobServiceClientCloud.getContainerClient(containerName);
 // </snippet_get_client>
 
 // <snippet_isStorageConfigured>
 // Feature flag - disable storage feature to app if not configured
-export const isStorageConfigured = () => {
-  return !storageAccountName || !sasToken ? false : true;
-};
+
+//Required? SasToken Is Actually optional when using MI 
+// export const isStorageConfigured = () => {
+//   return !storageAccountName || !sasToken ? false : true;
+// };
 // </snippet_isStorageConfigured>
 
 // <snippet_getBlobsInContainer>
 // return list of blobs in container to display
-export const getBlobsInContainer = async () => {
-  const returnedBlobUrls = [];
+// export const getBlobsInContainer = async () => {
+//   const returnedBlobUrls = [];
 
-  // get list of blobs in container
-  // eslint-disable-next-line
-  for await (const blob of containerClient.listBlobsFlat()) {
-    console.log(`${blob.name}`);
+//   // get list of blobs in container
+//   // eslint-disable-next-line
+//   for await (const blob of containerClient.listBlobsFlat()) {
+//     console.log(`${blob.name}`);
 
-    const blobItem = {
-      url: `https://${storageAccountName}.blob.core.windows.net/${containerName}/${blob.name}?${sasToken}`,
-      name: blob.name,
-    };
+//     const blobItem = {
+//       url: `https://${storageAccountName}.blob.core.windows.net/${containerName}/${blob.name}?${sasToken}`,
+//       name: blob.name,
+//     };
 
-    // if image is public, just construct URL
-    returnedBlobUrls.push(blobItem);
-  }
+//     // if image is public, just construct URL
+//     returnedBlobUrls.push(blobItem);
+//   }
 
-  return returnedBlobUrls;
-};
+//   return returnedBlobUrls;
+// };
+
 // </snippet_getBlobsInContainer>
 
 // <snippet_createBlobInContainer>
@@ -85,3 +93,30 @@ const uploadFileToBlob = async (file, selectedOption) => {
 // </snippet_uploadFileToBlob>
 
 export default uploadFileToBlob;
+
+
+
+// Example of Mickey Soft Code
+// const { DefaultAzureCredential } = require("@azure/identity");
+// const { BlobServiceClient } = require("@azure/storage-blob");
+// const defaultAzureCredential = new DefaultAzureCredential();
+
+// // Some code omitted for brevity.
+
+// async function uploadBlob(accountName, containerName, blobName, blobContents) {
+//     const blobServiceClient = new BlobServiceClient(
+//         `https://${accountName}.blob.core.windows.net`,
+//         defaultAzureCredential
+//     );
+
+//     const containerClient = blobServiceClient.getContainerClient(containerName);
+
+//     try {
+//         await containerClient.createIfNotExists();
+//         const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+//         const uploadBlobResponse = await blockBlobClient.upload(blobContents, blobContents.length);
+//         console.log(`Upload block blob ${blobName} successfully`, uploadBlobResponse.requestId);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
